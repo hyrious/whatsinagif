77d1d9789479c3739d8986a2e4951429a071e654
hyrious <hyrious@outlook.com>
2023-07-02 11:42:11 +0800

    add decoded image

--- code.js
+++ code.js
@@ -189,16 +189,19 @@ function decode_(buffer) {
   var transparent_index = -1
   var disposal          = 0
   var loop_count        = -1
+  var eof               = false
 
-decoding:
-  while (p < u8.byteLength) {
+  while (!eof && p < u8.byteLength) {
     var block_start = p
+
     switch (u8[p++]) {
       // - Extensions
       case 0x21: {
         if (!(p < u8.byteLength)) {
           throw indexError(p, [0, u8.byteLength].join('...'))
         }
+
+        var text = 'comment'
         switch (u8[p++]) {
           // - Application Extension
           case 0xff: {
@@ -208,7 +211,14 @@ decoding:
             }
             var block_type = str(u8, p, block_size); p += block_size
             if (block_type !== 'NETSCAPE2.0') {
-              throw argumentError('Invalid application block type', '"NETSCAPE2.0"', JSON.stringify(block_type))
+              console.warn('Invalid application block type, expecting "NETSCAPE2.0", got ' + JSON.stringify(block_type))
+              // Skip unknown app extension
+              var sub_block_size = 0
+              do {
+                sub_block_size = uchar(u8, p); p += 1
+                p += sub_block_size
+              } while (sub_block_size > 0)
+              break
             }
             var sub_block_size = uchar(u8, p); p += 1
             if (sub_block_size !== 0x03) {
@@ -251,13 +261,14 @@ decoding:
 
           // - Text, Comment
           case 0x01:
+            text = 'text'
           case 0xfe: {
             var block_size = 0
             do {
               block_size = uchar(u8, p); p += 1
               p += block_size
             } while (block_size > 0)
-            push_block(u8[block_start + 1] === 0xfe ? 'comment' : 'text', block_start, p)
+            push_block(text, block_start, p)
             break
           }
 
@@ -284,7 +295,7 @@ decoding:
         if (local_palette_flag) {
           palette_offset = p
           palette_size = num_local_colors
-          p += num_global_colors * 3
+          p += num_local_colors * 3
           push_block('local-palette', palette_offset, p)
         }
 
@@ -310,11 +321,14 @@ decoding:
       // - Trailer
       case 0x3b: {
         push_block('trailer', p - 1, p)
-        break decoding
+        eof = true
+        break
       }
 
       default: {
-        throw argumentError('Unknown GIF block', [0x21,0x2c,0x3b], u8[p - 1])
+        window.debug = {width, height, frames, p, info}
+        throw argumentError('Unknown GIF block', [0x21,0x2c,0x3b], '[' + (p - 1) + ']=' + u8[p - 1])
+        break
       }
     }
   }
@@ -352,6 +366,7 @@ function GifFrame(x, y, width, height,
 function decode(buffer) {
   try {
     target = null
+    decoding = -1
     gif = decode_(buffer)
     console.log(gif)
 
@@ -388,8 +403,14 @@ var $path = document.createElementNS("http://www.w3.org/2000/svg", 'path')
 $path.setAttribute('stroke', 'currentColor')
 $svg.appendChild($path)
 
+var $canvas = h('canvas', { id: 'frame' }),
+    decoding = -1
+
 function refresh() {
   $output.appendChild($svg)
+  $output.appendChild($canvas)
+  $canvas.width  = gif.width
+  $canvas.height = gif.height
 
   var $left   = $output.appendChild(h('div', { id: 'tree' }))
 
@@ -409,12 +430,19 @@ function refresh() {
     $loop.hidden = true
   }
 
+  var disposals = [
+    'clear', // 0 - clear the canvas
+    'keep',  // 1 - keep last canvas
+    'bg',    // 2 - fill canvas with background color
+    'last'   // 3 - restore to nearest frame with disposal = 1
+  ]
+
   for (var i = 0; i < gif.frames.length; ++i) {
     var frame = gif.frames[i]
     var $frame = $left.appendChild(h('a', { href: 'javascript:void 0' }))
     $frame.textContent =
       '#' + i + ' (' + [frame.x, frame.y, frame.width, frame.height] + ')\n' +
-      'disposal: ' + frame.disposal + ', delay: ' +
+      'disposal: ' + disposals[frame.disposal] + ', delay: ' +
       (frame.delay >= 100 ? (frame.delay / 100) + 's' : (frame.delay * 10) + 'ms')
     link_frame($frame, i)
   }
@@ -499,6 +527,8 @@ function update() {
 
   if (target === null) {
     $svg.style.display = 'none'
+    set_decoding(-1)
+    update_decoding()
     return
   }
 
@@ -513,6 +543,8 @@ function update() {
   var at   = parseInt(loc[1])
 
   if (type === 'frame') {
+    set_decoding(at)
+
     var $blocks = document.querySelectorAll('[data-type="image-descriptor"]')
     var $desc   = $blocks[at]
     var $end    = $desc
@@ -540,6 +572,8 @@ function update() {
       'V', end2.top + end2.height - 25
     ]
   } else {
+    set_decoding(-1)
+
     var size = parseInt(loc[2])
 
     var $view = document.querySelector('[data-type="' + type + '"]')
@@ -573,4 +607,229 @@ function update() {
   if ($path.getAttribute('d') !== d) {
     $path.setAttribute('d', d)
   }
+
+  update_decoding()
+}
+
+function set_decoding(index) {
+  decoding = index
+}
+
+var last_decoding = -2
+
+function update_decoding() {
+  if (last_decoding !== decoding) {
+    last_decoding = decoding
+    $canvas.width = $canvas.width  // this clears the canvas
+    if (decoding >= 0) {
+      try {
+        decode_frame()
+      } catch (err) {
+        console.error(err)
+        alert(err + '')
+      }
+    }
+  }
+}
+
+function decode_frame() {
+  var frame = gif.frames[decoding]
+
+  var x      = frame.x
+  var y      = frame.y
+  var width  = frame.width
+  var height = frame.height
+  var min_size    = gif.data[frame.data_offset]
+  var code_stream = make_bits_stream(gif.data.subarray(
+    frame.data_offset + 1,
+    frame.data_offset + frame.data_size
+  ))
+
+  if (!(2 <= min_size && min_size <= 8)) {
+    throw argumentError('Invalid LZW min code size', '2..8', min_size)
+  }
+
+  console.time('lzw')
+
+  var clear = 1 << min_size
+  var stop  = clear + 1
+  var size  = min_size + 1
+  var len   = stop + 1
+  var table = new Array(len)
+  for (var i = 0; i < clear + 2; ++i) { table[i] = [i] }
+
+  var j = 0, pixels = width * height, output = new Array(pixels),
+      last = null, init = false
+  while (j < pixels) {
+    if (init) {
+      size = min_size + 1
+      len  = stop + 1
+      init = false
+    }
+
+    var code  = code_stream(size)
+    var entry = null
+
+    if (code === clear) {
+      init = true
+      last = null
+      continue
+    }
+
+    else if (code === stop) {
+      break
+    }
+
+    // if new code in table, write out {code}
+    else if (code < len) {
+      entry = table[code]
+    }
+
+    // if new code not in table, write out {code-1}, {code-1}[0]
+    else if (code >= len) {
+      if (last !== null) {
+        entry = last.concat(last.slice(0, 1))
+      } else {
+        console.debug('invalid code:', code)
+        continue
+      }
+    }
+
+    else {
+      console.debug('invalid code:', code)
+      continue
+    }
+
+    for (var i = 0; i < entry.length; ++i) {
+      if (j < pixels) {
+        output[j++] = entry[i]
+      }
+    }
+
+    if (j >= pixels) {
+      break
+    }
+
+    // if {code-1} exist, add new entry {code-1} + {code or code-1}[0]
+    if (last !== null) {
+      table[len++] = last.concat(entry.slice(0, 1))
+    }
+
+    last = entry
+
+    if (len >= (1 << size)) {
+      if (size === 12) {
+        // special case: if run out of size, should add a clear command if not provided
+        if (code_stream.peek(size) !== clear) {
+          init = true
+        }
+      } else {
+        size += 1
+      }
+    }
+  }
+
+  console.timeEnd('lzw')
+  console.time('paint')
+
+  var palette = gif.data.subarray(
+    frame.palette_offset,
+    frame.palette_offset + frame.palette_size * 3
+  )
+  var interlace = frame.interlace
+  var transparent_index = frame.transparent_index
+
+  var ctx        = $canvas.getContext('2d')
+  var imageData  = ctx.createImageData(gif.width, gif.height)
+  var data       = imageData.data
+  var stride     = gif.width - width // pixels to skip
+  var scanstride = stride * 4        // bytes  to skip
+  var xleft      = width             // pixels to draw
+  var opbeg      = ((y * gif.width) + x) * 4
+  var opend      = ((y + height) * gif.width + x) * 4
+  var op         = opbeg
+
+  if (interlace > 0) {
+    scanstride += gif.width * 4 * 7  // skip 7 rows
+  }
+
+  var interlaceskip = 8
+  for (var i = 0; i < pixels; ++i) {
+    var index = output[i]
+
+    if (xleft === 0) {
+      op += scanstride
+      xleft = width
+      if (op >= opend) {
+        scanstride = stride * 4 + gif.width * 4 * (interlaceskip - 1)
+        op = opbeg + gif.width * (interlaceskip << 1)
+        interlaceskip >>= 1
+      }
+    }
+
+    if (index === transparent_index) {
+      op += 4
+    } else {
+      var r = palette[index * 3]
+      var g = palette[index * 3 + 1]
+      var b = palette[index * 3 + 2]
+      data[op++] = r
+      data[op++] = g
+      data[op++] = b
+      data[op++] = 255
+    }
+    xleft--
+  }
+
+  ctx.putImageData(imageData, 0, 0)
+
+  console.timeEnd('paint')
+}
+
+// The input is [bytes, ..., bytes, ..., 0x00],
+// only keep the '...' and make some helpers to read by bits
+function make_bits_stream(blocks) {
+  var p = 0, block_size = 0, i = 0, chunk, cur = 0, cur_bits = 0
+
+  function next_chunk() {
+    block_size = blocks[p++]
+    chunk = blocks.subarray(p, p + block_size)
+    p += block_size
+    i = 0
+    return block_size
+  }
+
+  var eof = next_chunk() === 0
+
+  function next_byte() {
+    if (eof) return;
+    if (i === chunk.byteLength) {
+      eof = next_chunk() === 0
+    }
+    if (!eof && i < chunk.byteLength) {
+      cur |= chunk[i++] << cur_bits
+      cur_bits += 8
+    }
+  }
+
+  function read(n_bits) {
+    while (!eof && cur_bits < n_bits) {
+      next_byte()
+    }
+    var code = cur & ((1 << n_bits) - 1)
+    cur >>= n_bits
+    cur_bits = Math.max(cur_bits - n_bits, 0)
+    return code
+  }
+
+  function peek(n_bits) {
+    while (!eof && cur_bits < n_bits) {
+      next_byte()
+    }
+    return cur & ((1 << n_bits) - 1)
+  }
+
+  read.peek = peek
+
+  return read
 }
